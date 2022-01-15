3dc213b10deee25b5ce57691bcd2db368b992483
MINJU-KIMmm <0112km@ewhain.net>
2022-01-16 01:12:13 +0900

    3-10 5단계

--- mysite/pybo/models.py
+++ mysite/pybo/models.py
@@ -19,3 +19,13 @@ class Answer(models.Model):
     content = models.TextField()
     create_date = models.DateTimeField()
     modify_date = models.DateTimeField(null=True, blank=True)
+
+#댓글 모델
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
+
--- mysite/pybo/views.py
+++ mysite/pybo/views.py
@@ -4,7 +4,7 @@ from .models import Question
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Question
 from django.utils import timezone
-from .forms import QuestionForm, AnswerForm
+from .forms import QuestionForm, AnswerForm, CommentForm
 from django.core.paginator import Paginator
 from django.contrib.auth.decorators import login_required
 from django.contrib import messages
@@ -143,3 +143,23 @@ def answer_delete(request, answer_id):
     else:
         answer.delete()
     return redirect('pybo:detail', question_id=answer.question.id)
+
+@login_required(login_url='comment:login')
+def comment_create_question(request, question_id):
+    """
+    pybo 질문 댓글 등록
+    """
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method=="POST":
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.author = request.user
+            comment.create_date = timezone.now()
+            comment.question = question
+            comment.save()
+            return redirect('pybo:detail', question_id=question.id)
+    else:
+        form = CommentForm()
+    context = {'form' : form}
+    return render(request, 'pybo/comment_form.html', context)
